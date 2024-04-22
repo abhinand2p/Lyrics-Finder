ea9c5bcbbcc29777d35981899b4686915f030cb7
Abhinand P <abhinandp@Abhinands-MacBook-Pro.local>
2024-04-22 23:12:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from  'axios';
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [artist, setArtist] = useState("");
+    const [song, setSong] = useState("");
+    const [lyrics, setLyrics] = useState("");
+
+    function searchLyrics() {
+      if( artist.trim() === "" && song.trim() === "" ){
+        alert("Please enter both Artist and Song name");
+      }else if(artist.trim() === ""){
+        alert("Please enter the Artist Name");
+      }else if(song.trim() === ""){
+        alert("Please enter the song name");
+      }else{
+      axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`).then(res => {
+        console.log(res.data.lyrics);
+        setLyrics(res.data.lyrics);
+    })
+    .catch(error => {
+      console.error("Error fetching lyrics:" , error);
+      setLyrics("Lyrics not found, please try again!");
+    });
+  }
+}
+return (
+  <div className='App'>
+    <h1> Lyrics Finder</h1>
+
+    <input className='inp' type="text" 
+    placeholder='Artist' onChange={ (e) => {setArtist(e.target.value) }}/>
+
+    <input className='inp' type="text" 
+    placeholder='song' onChange={ (e) => {setSong(e.target.value) }}/>
+
+    <button className='btn' onClick={ () => searchLyrics() }>Search</button>
+
+    <hr/>
+    <pre>{lyrics}</pre>
+
+  </div>
+);
+
 }
 
 export default App;
